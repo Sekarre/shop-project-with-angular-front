059f9305415e3abd825642089f9de5fcf6e20330
Sekarre <zimny2121@gmail.com>
2021-06-06 17:09:02 +0200

    Added images, and product routes with categories

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId!: number
 
-  constructor(private productService: ProductService) { }
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    if (this.route.snapshot.paramMap.has('id')) {
+      //get the "id" param string. convert string to a number using the "+" symbol
+      // @ts-ignore
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } else {
+      //default category id = 1
+      this.currentCategoryId = 1;
+    }
+
+    //get products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
-    )
+    );
   }
 
 }
